b79fee482d3af12e8c0c6571ec2663509f8bce1d
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2024-09-10 16:39:19 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+// put function declarations here:
+void helloWorld(int, String);
+int pin = 2;
+void setup() {
+  pinMode(pin, OUTPUT);
+  Serial.begin(921600);
+  Serial.printf("Initial Setup Complete\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String message = "Hello World";
+  helloWorld(pin, message);
+}
+
+// put function definitions here:
+void helloWorld(int pin, String message) {
+  delay(1000);
+  digitalWrite(pin, HIGH);
+  Serial.println(message);
+  delay(1000);
+  digitalWrite(pin, LOW);
+}
